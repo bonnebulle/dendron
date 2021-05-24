a4cc6d5d95c247696b39fb63987a6d2b6904bf9f
code-p8-2021 <vincent.bonnefille@gmail.com>
2021-05-24 14:13:17 +0200

    @ QuickPush -- lun. 24 mai 2021 - 14:13

--- @Notes_instructions/git_push.sh
+++ @Notes_instructions/git_push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cd /home/pi/Dendron/vault/
-git add /home/pi/Dendron/vault/*
+cd /home/pi/Dendron/
+git add /home/pi/Dendron/*
 git commit -am "@ QuickPush -- `date +'%a %e %b %Y - %R'`"
 git push
 echo "\n\n\n# https://github.com/bonnebulle/dendron/commits/main\n\n\n `date +'%a %e %b %Y - %R'`\n\n"
