aeacf8b68968e1fae7cf0d00ed248050be5a55eb
vb078 <vincent.bonnefille@gmail.com>
2021-05-26 18:49:01 +0200

    rm @Notes_instructions

--- @Notes_instructions/git_push.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /home/pi/Dendron/
-git add /home/pi/Dendron/*
-git commit -am "@ QuickPush -- `date +'%a %e %b %Y - %R'`"
-git push
-echo "\n\n\n# https://github.com/bonnebulle/dendron/commits/main\n\n\n `date +'%a %e %b %Y - %R'`\n\n"
